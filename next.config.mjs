d531fbf1dfb0da91041963fd1dfe2892a5ea62a3
sumail <sumail@xyzzdev.com>
2025-12-01 20:47:00 +0800

    feat: build test

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
  * @Author: sumail sumail@xyzzdev.com
  * @Date: 2024-07-02 16:10:47
  * @LastEditors: sumail sumail@xyzzdev.com
- * @LastEditTime: 2025-10-27 18:00:08
+ * @LastEditTime: 2025-12-01 20:45:45
  * @FilePath: /nextjs/travel-dairy/next.config.mjs
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -13,32 +13,34 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
 const nextConfig = {
-   reactStrictMode: false,
-   webpack: (config, options) => {
-      console.log(process.env.NEXT_PUBLIC_APPWRITE_DATABASE, 'hahahah')
-      config.module.rules.push({
-         test: /\.mp3$/,
-         use: {
-           loader: 'file-loader',
-         },
-       });
+  reactStrictMode: false,
+  output: 'export', // 关键配置：开启静态导出
 
-       config.resolve.alias = {
-        ...config.resolve.alias,
-        '@theme': path.resolve(__dirname, `src/themes/${process.env.THEME}`),
-      };
-       return config;
-   },
-   images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'avatars.githubusercontent.com',
-        },
-      ],
-    },
+  webpack: (config, options) => {
+    console.log(process.env.NEXT_PUBLIC_APPWRITE_DATABASE, 'hahahah');
+    config.module.rules.push({
+      test: /\.mp3$/,
+      use: {
+        loader: 'file-loader',
+      },
+    });
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@theme': path.resolve(__dirname, `src/themes/${process.env.THEME}`),
+    };
+    return config;
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+    ],
+    unoptimized: true, // GitHub Pages 不支持 Next 自带的图片优化，必须关闭
+  },
 };
 
 export default nextConfig;
